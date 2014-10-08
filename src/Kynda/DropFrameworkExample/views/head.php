7a69b82cab131ddb99d0bfc2aba66714e0e61b3e
Joseph Hallenbeck <joe@joehallenbeck.com>
2014-10-08 17:36:46 -0500

    Updated composer file to reflect name change

--- src/Kynda/DropFrameworkExample/views/head.php
+++ src/Kynda/DropFrameworkExample/views/head.php
@@ -13,8 +13,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
-    <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
-        
+    <link rel="stylesheet" type="text/css" href="/css/style.css" media="all" />        
+    
     <title><?= $title ?></title>
 </head>
 
